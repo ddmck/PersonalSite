b300e2ce047644f525f088fafa52936ceb8d8b4f
Donald <ddmckendrick@gmail.com>
2013-06-29 22:36:05 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DOnRails::Application.config.secret_token = 'b306120455f7389b7bd170e70d81d72350ccbb3d887e4006eec5207dfa3c69533d41209020a7b16da0f45bf45bc67f09569464bec8e690df8e8cb761e6112e65'
