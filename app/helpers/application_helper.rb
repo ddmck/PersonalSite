d493410b63147974bb5bc4944ea18591d97e6321
Donald <ddmckendrick@gmail.com>
2013-07-01 15:53:54 +0100

    Fix full titles and improve gitignore

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Give full title in browser
   def full_title(page_title)
-    base_title = "D on Rails"
+    base_title = "Donald on Rails"
     if page_title.empty?
       base_title
     else
